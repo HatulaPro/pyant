610999d6de6ceff135587dee2badf78513b1d20d
shalev280804@gmail.com <shalev280804@gmail.com>
2022-04-26 09:41:30 -0400

    Improved pixel interface

--- src/pyant/pixel.py
+++ src/pyant/pixel.py
@@ -4,15 +4,10 @@ import xtermcolor
 
 class Pixel:
     _character: str
-    foreground: int
-    background: int
+    _foreground: int
+    _background: int
 
     def __init__(self, character: str, foreground: int, background: int) -> None:
-        # if not isinstance(character, str):
-        #     raise TypeError('Character must be a string.')
-        # if len(character) != 1:
-        #     raise ValueError('Character must be a string of length one.')
-
         if not isinstance(foreground, int):
             raise TypeError('Foreground must be an integer.')
         if not isinstance(background, int):
@@ -27,12 +22,36 @@ class Pixel:
         return self._character
 
     @character.setter
-    def character(self, character: str) -> str:
+    def character(self, character: str):
         if not isinstance(character, str):
             raise TypeError('Character must be a string.')
         if len(character) != 1:
             raise ValueError('Character must be a string of length one.')
         self._character = character
+
+    @property
+    def background(self) -> int:
+        return self._background
+
+    @background.setter
+    def background(self, background: int):
+        if not isinstance(background, int):
+            raise TypeError('Background must be an integer.')
+        if background < 0 or background > 255:
+            raise ValueError('Background must be in range 0-255.')
+        self._background = background
+
+    @property
+    def foreground(self) -> int:
+        return self._foreground
+
+    @foreground.setter
+    def foreground(self, foreground: int):
+        if not isinstance(foreground, int):
+            raise TypeError('Foreground must be an integer.')
+        if foreground < 0 or foreground > 255:
+            raise ValueError('Foreground must be in range 0-255.')
+        self._foreground = foreground
 
     def _get_show_string(self) -> str:
         return f'\033[38;5;{self.foreground}m\033[48;5;{self.background}m{self.character}\033[m'
